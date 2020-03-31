829bd6509cb8a4a96fe0aec43ca24703712c01f9
Ali Salaheldin <rated.ali.7@gmail.com>
2020-03-31 09:37:51 +0200

    Fixes

--- bin/powersave.sh
+++ bin/powersave.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 /usr/bin/systemctl --user stop battery-monitor.timer
 /usr/bin/systemctl --user stop mpd.socket mpd.service
+/usr/bin/systemctl --user stop udiskie.service
 /usr/bin/systemctl stop bluetooth.service
 /usr/bin/systemctl stop NetworkManager.service
 /usr/bin/systemctl stop ModemManager.service
@@ -10,3 +11,6 @@
 /usr/bin/pkill kdeconnect-indi
 /usr/bin/pkill redshift
 /usr/bin/pkill nm-applet
+/usr/bin/pkill blueman-applet
+
+sudo /usr/bin/cpupower frequency-set --max 600MHz
