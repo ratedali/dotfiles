420b2fb9a1b25836686df0fb685cf7c013a01c27
Ali Salaheldin <rated.ali.7@gmail.com>
2025-06-14 00:20:39 +0200

    remove pscompletions temp, add shortcuts

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -2,3 +2,16 @@
 New-Alias -Name g -Value git
 New-Alias -Name v -Value notepad++.exe
 #endregion Aliases
+
+#region Shortcuts
+$global:workspace = "$HOME\source"
+function workspace
+{
+  cd $workspace
+}
+
+function psdir
+{
+  cd (Split-Path $PROFILE.CurrentUserCurrentHost)
+}
+#endregion
